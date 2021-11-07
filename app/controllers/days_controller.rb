30b72a6fd39687ae548bd0fc6cb62d78b32b0b15
kuzmich-1992 <kuzmichman92@gmail.com>
2021-11-07 09:52:54 +0200

    third_commit

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,5 +1,38 @@
-# frozen_string_literal: true
-
 class DaysController < ApplicationController
-  def index; end
+  def index
+    day = Day.all.order(created_at: :desc)
+    render json: day
+  end
+
+  def create
+    day = Day.create!(day_params)
+    if day
+      render json: day
+    else
+      render json: day.errors
+    end
+  end
+
+  def show
+    if day
+      render json: day
+    else
+      render json: day.errors
+    end
+  end
+
+  def destroy
+    day&.destroy
+    render json: { message: 'day deleted!' }
+  end
+
+  private
+
+  def day_params
+    params.permit(:name, :surname, :high_school_class)
+  end
+
+  def day
+    @day ||= Day.find(params[:id])
+  end
 end
--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -1,5 +1,43 @@
 # frozen_string_literal: true
 
 class WeeksController < ApplicationController
-  def index; end
+  # frozen_string_literal: true
+
+  def index
+    week = Week.all
+    render json: week
+  end
+
+  def create
+    week = Week.create!(week_params)
+    if week
+      render json: week
+    else
+      render json: week.errors
+    end
+  end
+
+  def show
+    if week
+      render json: week
+    else
+      render json: week.errors
+    end
+  end
+
+  def destroy
+    week&.destroy
+    render json: { message: 'week deleted!' }
+  end
+
+  private
+
+  def week_params
+    params.permit(:name, :surname, :high_school_class)
+  end
+
+  def day
+    @week ||= Week.find(params[:id])
+  end
 end
+
