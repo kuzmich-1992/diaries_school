bf4c8f00cc12aa8dc4af525af13a481cf11832d2
kuzmich-1992 <kuzmichman92@gmail.com>
2021-11-07 11:21:10 +0200

    third_commit

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,40 +1,40 @@
 # frozen_string_literal: true
 
 class DiariesController < ApplicationController
-  def index
-    diary = Diary.all.order(created_at: :desc)
-    # render json: diary
+  def new
+    @diary = Diary.new
   end
-
+  
   def create
-    diary = Diary.create!(diary_params)
-    if diary
-      render json: diary
+    @diary = Diary.new permitted_parameters
+    if @room.save
+      flash[:success] = "Diary #{@diary.name} was created successfully"
+      redirect_to diaries_path
     else
-      render json: diary.errors
+      render :new
     end
   end
 
-  def show
-    if diary
-      render json: diary
+  def edit
+  end
+    
+  def update
+    if @diary.update_attributes(permitted_parameters)
+      flash[:success] = "diary #{@diary.name} was updated successfully"
+      redirect_to diaries_path
     else
-      render json: diary.errors
+      render :new
     end
   end
-
-  def destroy
-    diary&.destroy
-    render json: { message: 'diary deleted!' }
+  
+  protected
+    
+  def load_entities
+    @diaries = Diary.all
+    @room = Diary.find(params[:id]) if params[:id]
   end
-
-  private
-
-  def diary_params
-    params.permit(:name, :surname, :high_school_class)
-  end
-
-  def diary
-    @diary ||= Diary.find(params[:id])
+    
+  def permitted_parameters
+    params.require(:diary).permit(:name, :surname, :class)
   end
 end
